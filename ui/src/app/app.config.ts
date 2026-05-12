6360b0474713a56ff1e106b8963acf4a69b7e874
Martin Magne <martin.magne@ipt.ch>
2026-05-12 13:47:23 +0200

    remove unused interceptor

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(),
     provideStore(),
     ...environment.providers,
   ],
